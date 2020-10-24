c2676b8f988a790de548d17c1d2fcaafad41861c
AinoMegumi <aimegu2014@gmail.com>
2020-10-24 23:25:24 +0900

    プロファイルページを作成

---
+++ ShortURLService/Models/ProfileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShortURLService.Models
+{
+    public class ProfileModel
+    {
+        public RedirectInformation RedirectInfo { get; private set; }
+        public ProfileModel(RedirectInformation Info)
+        {
+            RedirectInfo = Info;
+        }
+    }
+}
